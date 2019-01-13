f68e9476c28e498a958f86254cfcefc7b39c6018
zhiqiang zhang <zz373609@ohio.edu>
2019-01-13 11:30:04 +0800

    add the thing

--- frontend/src/routes/app/index.js
+++ frontend/src/routes/app/index.js
@@ -5,24 +5,28 @@ import { withRouter } from 'react-router'
 import { connect } from 'dva'
 import styles from './style/index.scss'
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       showTopNav: true
     }
   }
-  render () {
+  render() {
     const { history, homepage, dispatch } = this.props
     return <div className={styles.container} style={{
       width: '100vw',
       margin: '0 auto',
-      minWidth: '1024px',
       background: 'repeating-linear-gradient( -45deg, #f2f2f2, #f2f2f2 1px, #d9d9d9 1px, #fff 5px)',
       position: 'relative'
     }}>
       {this.state.showTopNav && <NavBar history={history} dispatch={dispatch} homepage={homepage} />}
       <TopBanner src={homepage.topSrc} />
-      {this.props.children}
+      <div style={{
+        maxWidth: '1920px',
+        margin: '0 auto'
+      }}>
+        {this.props.children}
+      </div>
       <BottomNav dispatch={dispatch} homepage={homepage} history={history} />
     </div>
   }
