e3d478218eff8b1a32ff402b742334cf46931a1b
zhiqiang zhang <zz373609@ohio.edu>
2019-01-20 14:13:10 +0800

    fix the to

--- frontend/src/components/ProjectMenu/index.js
+++ frontend/src/components/ProjectMenu/index.js
@@ -12,7 +12,7 @@ class ProjectMenu extends Component {
       transform: 'translateX(-50%)',
       display: visible ? 'block' : 'none'
     }}>
-      <div style={{ verticalAlign: 'middle', width: '23.36vw', height: '12.00vw', minWidth: 218, minHeight: 112, background: 'rgba(0,0,0,0.3)', borderRadius: '4px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ verticalAlign: 'middle', width: '250px', height: '150px', minWidth: 218, minHeight: 112, background: 'rgba(0,0,0,0.3)', borderRadius: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         {
           product.map((item, index) => {
             return <Project key={index} index={index} item={item} hidden={hidden} history={history} language={language} />
@@ -35,28 +35,29 @@ const Project = ({ item, history, hidden, language, index }) => {
     className={styles.project}
   >
     <div style={index === 0 ? {
-      height: '7.6vw',
-      width: '7.6vw',
+      height: '80px',
+      width: '80px',
       borderRadius: '50%',
       background: '#f1f2f3',
       position: 'relative'
     } : {
-        height: '7.6vw',
-        width: '7.6vw',
+        height: '80px',
+        width: '80px',
         borderRadius: '50%',
         background: '#f1f2f3',
         position: 'relative',
-        marginLeft: '10px'
-
+        marginLeft: '18px'
       }} >
       <div style={{
-        height: '7.6vw',
-        width: '7.6vw',
+        height: '80px',
+        width: '80px',
         borderRadius: '50%',
       }} className={styles.cover} />
       <img style={{ width: '100%', borderRadius: '50%' }} src={item.top_logo} />
     </div>
-    <span style={{ width: '100%', textAlign: 'center', marginTop: '8px', fontSize: '10px', fontWeight: '300' }}>{item.title[language]}</span>
+    <span style={index == 1 ? { width: '100%', textAlign: 'center', marginTop: '10px', fontSize: '10px', fontWeight: '300', marginLeft: '18px' } :
+      { width: '100%', textAlign: 'center', marginTop: '10px', fontSize: '10px', fontWeight: '300' }
+    }>{item.title[language]}</span>
   </div>
 }
 
